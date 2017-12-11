5a099710b31b6a9950f0bc3c0793a3a06e8ef9ef
LJ <1801926836@qq.com>
2017-12-11 08:47:53 +0800

    更新 ErpBankService.java

--- src/main/java/com/yidu/service/ErpBank/ErpBankService.java
+++ src/main/java/com/yidu/service/ErpBank/ErpBankService.java
@@ -17,4 +17,28 @@ public interface ErpBankService {
 	 * @return
 	 */
 	List<ErpBank> selectAll(Map<String,Object> map);
+	/**
+	 * 查询和显示的中行数
+	 * @param map 分页和模糊查询的参数
+	 * @return int
+	 */
+	int selectAllConut(Map<String,Object> map);
+	 /**
+     * 添加
+     * @param record 入库对象
+     * @return int
+     */
+    int insertSelective(ErpBank record,List<ErpInvedet> list,String quaId);
+    /**
+     * 查询单个对象
+     * @param bankId 入库id
+     * @return 入库对象
+     */ 
+    ErpBank selectByPrimaryKey(String bankId);
+    /**
+     * 修改入库
+     * @param record 入库id
+     * @return 入库对象
+     */
+    int updateByPrimaryKeySelective(ErpBank record);
 }
